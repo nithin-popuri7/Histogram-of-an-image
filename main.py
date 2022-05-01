ca725d44c5ac806f5a296db430e10f5851589b4c
Popuri Nithin <94154780+nithin-popuri7@users.noreply.github.com>
2022-05-01 16:53:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2
+import matplotlib.pyplot as plt
+
+gray_image = cv2.imread('sport.jpg',0)
+equ=cv2.equalizeHist(gray_image)
+cv2.imshow('Gray image',gray_image)
+cv2.imshow('Equalized Image',equ)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
